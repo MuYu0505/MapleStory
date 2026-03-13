31427e6b95d5bb8ced5b13a49e43bfeece3845c4
weipeng.chen <weipeng.chen@yisurvye.com>
2026-03-13 15:35:10 +0800

    Initial_commit_冒险岛角色创建系统重构完成

--- scripts/character_creation.gd
+++ scripts/character_creation.gd
@@ -1,24 +1,28 @@
 extends Control
 
 # 定义角色样貌索引
-var hair_idx = 0
-var face_idx = 0
+var hair_style_idx = 0
+var hair_color_idx = 0
 var skin_idx = 0
 
-# 样貌数据 (之后你可以把素材路径放在这里)
-var hairs = [null, null, null] # 先放空位，你可以填入 Texture2D
-var faces = [null, null, null]
-var skins = [Color.WHITE, Color.BISQUE, Color.SANDY_BROWN] # 肤色可以直接用颜色调制
+# 样貌数据
+var hair_styles = {} # 格式: {"style_name": [texture_v00, texture_v01, ...]}
+var hair_style_names = [] # 存储款式名称，方便索引
+# 肤色图片数组 (11 种肤色)
+var skin_textures = []
+
+# 裁剪设置 (根据你的图片素材调整)
+var sprite_width = 64  # 单个小人的宽度
+var sprite_height = 64 # 单个小人的高度
 
 var stats = {"str": 4, "dex": 4, "int": 4, "luk": 4}
 
 # 节点引用
-@onready var class_label = $VBoxContainer/ClassSelection/VBoxContainer/ClassName
-@onready var desc_label = $VBoxContainer/ClassSelection/VBoxContainer/ClassDesc
-# 样貌预览节点 (之后我们会把这些节点加到场景里)
-@onready var skin_preview = $VBoxContainer/CharacterPreview/Skin
-@onready var hair_preview = $VBoxContainer/CharacterPreview/Hair
-@onready var face_preview = $VBoxContainer/CharacterPreview/Face
+@onready var class_label = $VBoxContainer/ClassSelection/ClassName
+@onready var desc_label = $VBoxContainer/ClassSelection/ClassDesc
+# 样貌预览节点 (已改为 Sprite2D)
+@onready var skin_preview: Sprite2D = $VBoxContainer/CharacterPreview/Skin
+@onready var hair_preview: Sprite2D = $VBoxContainer/CharacterPreview/Hair
 
 @onready var str_val = $VBoxContainer/Stats/STR/Value
 @onready var dex_val = $VBoxContainer/Stats/DEX/Value
@@ -28,12 +32,21 @@ var stats = {"str": 4, "dex": 4, "int": 4, "luk": 4}
 @onready var start_button = $VBoxContainer/StartButton
 
 # 样貌控制按钮引用
-@onready var next_hair_btn = $VBoxContainer/AppearanceControls/HairControl/NextHair
-@onready var next_face_btn = $VBoxContainer/AppearanceControls/FaceControl/NextFace
+@onready var next_style_btn = $VBoxContainer/AppearanceControls/HairControl/NextHair
+@onready var next_color_btn = $VBoxContainer/AppearanceControls/HatControl/NextHat
 @onready var next_skin_btn = $VBoxContainer/AppearanceControls/SkinControl/NextSkin
 
 func _ready():
 	randomize()
+	
+	# 加载 11 种肤色图片
+	for i in range(11):
+		var path = "res://assets/skin/skin_" + str(i) + ".png"
+		skin_textures.append(load(path))
+	
+	# 分类加载发型素材
+	load_hair_assets()
+	
 	# 初始设为“新手”
 	if class_label: class_label.text = "新手 (Beginner)"
 	if desc_label: desc_label.text = "初出茅庐的冒险者。达到10级后可转职。"
@@ -44,42 +57,93 @@ func _ready():
 	# 按钮信号连接
 	start_button.pressed.connect(_on_start_button_pressed)
 	dice_button.pressed.connect(_on_dice_button_pressed)
-	next_hair_btn.pressed.connect(_on_next_hair_pressed)
-	next_face_btn.pressed.connect(_on_next_face_pressed)
+	next_style_btn.pressed.connect(_on_next_style_pressed)
+	next_color_btn.pressed.connect(_on_next_color_pressed)
 	next_skin_btn.pressed.connect(_on_next_skin_pressed)
 
-# 点击开始游戏
-func _on_start_button_pressed():
-	print("开始游戏！属性：", stats, " 样貌索引：", [hair_idx, face_idx, skin_idx])
-	# 之后可以在这里切换到游戏主场景
+# 核心逻辑：自动分类加载发型
+func load_hair_assets():
+	var path = "res://assets/hair/"
+	var dir = DirAccess.open(path)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if not dir.current_is_dir() and file_name.ends_with(".png"):
+				# 解析文件名，例如: char_a_p1_4har_bob1_v00.png
+				# 我们需要提取款式(bob1)
+				var parts = file_name.split("_")
+				if parts.size() >= 5:
+					var style = parts[4] # bob1 或 dap1
+					var texture = load(path + file_name)
+					
+					if not hair_styles.has(style):
+						hair_styles[style] = []
+						hair_style_names.append(style)
+					
+					hair_styles[style].append(texture)
+			file_name = dir.get_next()
+	
+	# 对每个款式的颜色进行排序，确保 v00, v01 顺序正确
+	for style in hair_styles:
+		hair_styles[style].sort_custom(func(a, b): return a.resource_path < b.resource_path)
 
 # 随机样貌
 func randomize_appearance():
-	hair_idx = randi() % hairs.size()
-	face_idx = randi() % faces.size()
-	skin_idx = randi() % skins.size()
+	if hair_style_names.size() > 0:
+		hair_style_idx = randi() % hair_style_names.size()
+		var current_style = hair_style_names[hair_style_idx]
+		hair_color_idx = randi() % hair_styles[current_style].size()
+		
+	if skin_textures.size() > 0: 
+		skin_idx = randi() % skin_textures.size()
+	
 	update_appearance_ui()
 
-# 切换样貌的函数 (你可以绑定到 UI 按钮上)
-func _on_next_hair_pressed():
-	hair_idx = (hair_idx + 1) % hairs.size()
-	update_appearance_ui()
+# 切换发型款式
+func _on_next_style_pressed():
+	if hair_style_names.size() > 0:
+		hair_style_idx = (hair_style_idx + 1) % hair_style_names.size()
+		# 切换款式时，重置颜色索引，防止溢出
+		var current_style = hair_style_names[hair_style_idx]
+		hair_color_idx = hair_color_idx % hair_styles[current_style].size()
+		update_appearance_ui()
 
-func _on_next_face_pressed():
-	face_idx = (face_idx + 1) % faces.size()
-	update_appearance_ui()
+# 切换发型颜色
+func _on_next_color_pressed():
+	if hair_style_names.size() > 0:
+		var current_style = hair_style_names[hair_style_idx]
+		hair_color_idx = (hair_color_idx + 1) % hair_styles[current_style].size()
+		update_appearance_ui()
 
 func _on_next_skin_pressed():
-	skin_idx = (skin_idx + 1) % skins.size()
-	update_appearance_ui()
+	if skin_textures.size() > 0:
+		skin_idx = (skin_idx + 1) % skin_textures.size()
+		update_appearance_ui()
 
 # 更新样貌显示
 func update_appearance_ui():
-	if hair_preview: hair_preview.texture = hairs[hair_idx]
-	if face_preview: face_preview.texture = faces[face_idx]
-	if skin_preview: 
-		# 肤色可以通过改变 TextureRect 的 modulate 颜色来实现
-		skin_preview.modulate = skins[skin_idx]
+	# 设置发型预览 (款式 + 颜色)
+	if hair_preview and hair_style_names.size() > 0:
+		var current_style = hair_style_names[hair_style_idx]
+		var textures = hair_styles[current_style]
+		hair_preview.texture = textures[hair_color_idx]
+		hair_preview.region_enabled = true
+		hair_preview.region_rect = Rect2(0, 0, sprite_width, sprite_height)
+		
+	# 设置肤色预览
+	if skin_preview and skin_textures.size() > 0: 
+		skin_preview.texture = skin_textures[skin_idx]
+		skin_preview.region_enabled = true
+		skin_preview.region_rect = Rect2(0, 0, sprite_width, sprite_height)
+
+# 点击开始游戏
+func _on_start_button_pressed():
+	var current_style = "无"
+	if hair_style_names.size() > 0:
+		current_style = hair_style_names[hair_style_idx]
+	print("开始游戏！属性：", stats, " 样貌：款式-", current_style, " 颜色索引-", hair_color_idx, " 肤色索引-", skin_idx)
+	# 之后可以在这里切换到游戏主场景
 
 # 点击骰子随机属性
 func _on_dice_button_pressed():
